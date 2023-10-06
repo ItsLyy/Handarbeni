358a56807659b66abe125e7ee46ba82357c5bba7
EgoLync <irlyfizaharis@gmail.com>
2023-10-06 18:44:42 +0700

    Menambahkan File

---
+++ dist/js/script.js
@@ -0,0 +1,40 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Contact
+const button = document.querySelector("#button-logo");
+const contact = document.querySelector("#contact-logo");
+const cancel = document.querySelector("#x-logo");
+const media = document.querySelector("#media-logo");
+
+button.addEventListener("click", function () {
+  contact.classList.toggle("contact-active");
+  cancel.classList.toggle("x-active");
+  console.log("WOI");
+});
